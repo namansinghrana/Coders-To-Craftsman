6485cc499cb7dde491d4cd6c3c5c5fb78911d8ed
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-10-06 06:55:36 +0000

    Finally applied the final layer of abstraction to the Main program while adding support for the system crash by implementing try catch.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,48 @@
 import java.util.*;
 
+//for this step we are going to apply 
+//Exception handling and continue the factory method implementation.
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        OrderService os = new OrderService();
+        Scanner scanner = new Scanner(System.in);
+        OrderService orderService = new OrderService();
         while(true){
             System.out.println("1.Add order  2.View orders  3.Total price  4.Exit");
-            int ch = Integer.parseInt(sc.nextLine());
+            int ch = Integer.parseInt(scanner.nextLine());
             if(ch==1){
                 System.out.print("Enter item name: ");
-                String item = sc.nextLine();
+                String item = scanner.nextLine();
                 System.out.print("Enter price: ");
-                double price = Double.parseDouble(sc.nextLine());
+                double price = Double.parseDouble(scanner.nextLine());
                 System.out.print("Enter quantity: ");
-                int qty = Integer.parseInt(sc.nextLine());
-                os.addOrder(item, price, qty);
+                int qty = Integer.parseInt(scanner.nextLine());
+                
+                //New implement after orderfactory
+                try {
+                    Order order = OrderFactory.createOrder(item, price, qty);
+                    orderService.addOrder(order);
+                    System.out.println("Order Added Successfully"); 
+            
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Error" + e.getMessage());
+                }
+                
             } else if(ch==2){
-                os.showOrders();
+
+                //New implement after orderfactory
+                try { 
+                    orderService.showOrders();
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Error" + e.getMessage());
+                }
             } else if(ch==3){
-                System.out.println("Total: "+os.total());
+                
+                //New implement after orderfactory
+                try{
+                    System.out.println("Total: "+orderService.total());
+                }catch(IllegalArgumentException e){
+                    System.out.println("Error" + e.getMessage());
+                }
             } else if(ch==4){
                 System.out.println("Bye");
                 break;
