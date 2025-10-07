0909a760b2364063e920057c1be7d1c9e166c1c2
Naman Singh Rana <101310228+namansinghrana@users.noreply.github.com>
2025-10-07 23:21:14 +0530

    Removed improper instantiation of DiscountStrategy.

--- actOne/src/OrderService.java
+++ actOne/src/OrderService.java
@@ -8,43 +8,47 @@ import discount.DiscountStrategy;
 public class OrderService {
     List<Order> orders = new ArrayList<>();
 
-    //Creating the discountStrategy button on my app.
-    private DiscountStrategy discountStrategy = new DiscountStrategy();
+    // Creating the discountStrategy button on my app.
+    private DiscountStrategy discountStrategy;
 
-    //Updates Constructor
-    public OrderService(DiscountStrategy discountStrategy){
+    private List<Observer> observers = new ArrayList<>();
+
+    // Updates Constructor
+    public OrderService(DiscountStrategy discountStrategy) {
         this.discountStrategy = discountStrategy;
         this.orders = new ArrayList<>();
     }
 
-    //Changing strategy in real-Time
-    public void setDiscountStrategy(DiscountStrategy discountStrategy){
+    // Changing strategy in real-Time
+    public void setDiscountStrategy(DiscountStrategy discountStrategy) {
         this.discountStrategy = discountStrategy;
     }
 
-    //Previously We were just creating raw objects here in OrderService
-    //But now we moved the object creation to the OrderFactory
-    //Now we dont have to create objects at multiple layers
-    //And we can just use an objectfactory to create new objects. 
-    public void addOrder(Order order){
+    // Previously We were just creating raw objects here in OrderService
+    // But now we moved the object creation to the OrderFactory
+    // Now we dont have to create objects at multiple layers
+    // And we can just use an objectfactory to create new objects.
+    public void addOrder(Order order) {
         orders.add(order);
     }
 
-    //Made the same changes across showOrders
-    public void showOrders(){
-        for(Order order : orders){
-            System.out.println(order.getItem()+" x"+order.getQuantity()+" @ "+order.getPrice());
+    // Made the same changes across showOrders
+    public void showOrders() {
+        for (Order order : orders) {
+            System.out.println(order.getItem() + " x" + order.getQuantity() + " @ " + order.getPrice());
         }
     }
 
-    //Made the same changes across total as well
-    public double total(){
-        double total =0;
-        //did forgot to update the newly created core
-        //logic for calculating individual total for order.
-        for(Order order: orders){ total += order.calculateTotal(); }
-        //Deleted the random discounting 
-        //& aaplied the new discount strategy.
+    // Made the same changes across total as well
+    public double total() {
+        double total = 0;
+        // did forgot to update the newly created core
+        // logic for calculating individual total for order.
+        for (Order order : orders) {
+            total += order.calculateTotal();
+        }
+        // Deleted the random discounting
+        // & aaplied the new discount strategy.
         return discountStrategy.applyDiscount(total);
     }
 }
